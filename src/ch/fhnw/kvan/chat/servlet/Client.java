5f7653d916dc39d5d8a61a04c24985b924f34d89
Daniel <dforrer@gmail.com>
2015-05-02 18:33:42 +0200

    almost working perfectly

--- src/ch/fhnw/kvan/chat/servlet/Client.java
+++ src/ch/fhnw/kvan/chat/servlet/Client.java
@@ -3,11 +3,13 @@ package ch.fhnw.kvan.chat.servlet;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 import ch.fhnw.kvan.chat.general.Chats;
 import ch.fhnw.kvan.chat.general.Participants;
@@ -19,8 +21,8 @@ import ch.fhnw.kvan.chat.utils.*;
 public class Client implements IChatDriver, IChatRoom {
 
 	private static Client client;
-
-	private static URL url;
+	private static String clientName;
+	private static String url;
 	private boolean running;
 	private ClientGUI gui;
 
@@ -33,161 +35,119 @@ public class Client implements IChatDriver, IChatRoom {
 		// Start GUI
 	}
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		System.out.println("Client-Class has been called with arguments: ");
 		System.out.println("Name: " + args[0]);
 		System.out.println("Host: " + args[1]);
 
-		String clientName = args[0];
+		clientName = args[0];
+		url = "http://" + args[1];
+
+		client = new Client();
+
+		client.gui = new ClientGUI(client, clientName);
+
+		// Send name
+
+		client.addParticipant(clientName);
+
+		// Get topics
+		client.getTopics();
+		client.getParticipants();
+		client.getMessages(client.gui.getCurrentTopic());
+	}
+
+	/*
+	 * "?action=addParticipant&name=<Name>"
+	 * "?action=removeParticipant&name=<Name>" "?action=addTopic&topic=<Thema>"
+	 * "?action=removeTopic&topic=<Thema>"
+	 * "?action=postMessage&message=<Nachricht>&topic=<Thema>"
+	 * ￼"?action=getMessages&topic=<Thema>" ￼"?action=getTopics"
+	 * "?action=refresh&topic=<Thema>"
+	 */
+
+	/**
+	 * Expects a URLEncoder.encode(topic, "UTF-8")-encoded string as "request"
+	 * 
+	 * @param request
+	 * @return
+	 */
+	public String makeRequest(String request) {
+		System.out.println("request:" + request);
+		URL url_temp = null;
 		try {
-			url = new URL("http://"+args[1]);
-		} catch (MalformedURLException e) {
+			url_temp = new URL(url + request);
+		} catch (MalformedURLException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-		
-		// Make request
-		
-		String response = null;
-		
+
 		try {
-			HttpURLConnection c = (HttpURLConnection) url.openConnection();
-	        c.setRequestMethod("GET");
-	        c.setRequestProperty("Content-length", "0");
-	        c.setUseCaches(false);
-	        c.setAllowUserInteraction(false);
-	        c.setConnectTimeout(30);
-	        c.setReadTimeout(30);
-	        c.connect();
-	        int status = c.getResponseCode();
-         	System.out.println("Status-Code:" + status);
-     	   
-	        switch (status) {
-	            case 200:	// notice the missing break!
-	            case 201:
-	                BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
-	                StringBuilder sb = new StringBuilder();
-	                String line;
-	                while ((line = br.readLine()) != null) {
-	                    sb.append(line+"\n");
-	                }
-	                br.close();
-	                response = sb.toString();
-	        }
-	        System.out.println(response);
+			HttpURLConnection c = (HttpURLConnection) url_temp.openConnection();
+			c.setRequestMethod("GET");
+			c.setRequestProperty("Content-length", "0");
+			c.setUseCaches(false);
+			c.setAllowUserInteraction(false);
+			c.setConnectTimeout(30);
+			c.setReadTimeout(30);
+			c.connect();
+			int status = c.getResponseCode();
+			System.out.println("Status-Code:" + status);
+
+			switch (status) {
+			case 200: // notice the missing break!
+			case 201:
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						c.getInputStream()));
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = br.readLine()) != null) {
+					sb.append(line);
+				}
+				br.close();
+				return sb.toString();
+			default:
+				System.out.println(request + "-request failed!!!");
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		client = new Client();
-	
-		//TODO Send name
-		
-		client.gui = new ClientGUI(client, clientName);
+		return null;
 	}
 
-	public void processServerMessage(String input) {
-		System.out.println("serverMessage:" + input);
-
-		// Handle empty response
-		if (input.equals("topics=") || input.equals("participants=")) {
-			return;
-		}
-
-		String key = input.split("=")[0];
-		String value = input.split("=")[1];
-
-		switch (key) {
-		case "message":
-			// FORMAT: "message=Hello World;topic=myTopic"
-			String topic = input.split("=")[2];
-			System.out.println("topic:" + topic);
-			String message = input.split("=")[1].split(";")[0];
-			System.out.println("message:" + message);
-			if (!chatInfo.addMessage(topic, message)) {
-				System.out.println("ERROR: Couldnt add message!");
-				break;
-			}
-			// Create String[] for the gui
-			// ("messages=String1;String2;String3;")
-			System.out.println(chatInfo.getMessages(topic));
-			String messages = chatInfo.getMessages(topic).split("=")[1];
-			String[] stringArray = messages.split(";;");
-
-			// Check if should update the gui
-			if (gui.getCurrentTopic().equals(topic)) {
-				gui.updateMessages(stringArray);
-			}
-
-			break;
-		case "add_topic":
-			// FORMAT: "add_topic=myTopic"
-			String addedTopic = input.split("=")[1];
-			chatInfo.addTopic(addedTopic);
-			gui.addTopic(addedTopic);
-			break;
-		case "remove_topic":
-			// FORMAT: "remove_topic=myTopic"
-			String removedTopic = input.split("=")[1];
-			System.out.println("removedTopic:" + removedTopic);
-			chatInfo.removeTopic(removedTopic);
-			gui.removeTopic(removedTopic);
-			break;
-		case "topics":
-			// FORMAT: "topics=Topic1;Topic2;"
-			String[] topics = value.split(";");
-			for (int i = 0; i < topics.length; i++) {
-				chatInfo.addTopic(topics[i]);
-			}
-			gui.updateTopics(topics);
-			break;
-		case "add_participant":
-			// FORMAT: "add_participant=client1"
-			String addedPart = input.split("=")[1];
-			participantInfo.addParticipant(addedPart);
-			gui.addParticipant(addedPart);
-			break;
-		case "remove_participant":
-			// FORMAT: "remove_participant=client2"
-			String removedPart = input.split("=")[1];
-			participantInfo.removeParticipant(removedPart);
-			gui.removeParticipant(removedPart);
-			break;
-		case "participants":
-			String[] parts = value.split(";");
-			for (int i = 0; i < parts.length; i++) {
-				participantInfo.addParticipant(parts[i]);
-			}
-			gui.updateParticipants(parts);
-			break;
-		case "messages":
-			// FORMAT: "messages=Meldung1;Meldung2;topic=myTopic"
-			String topic3 = input.split("=")[2];
-			System.out.println("topic:" + topic3);
-			String[] messages2 = input.split("=")[1].split(";;");
-			for (int i = messages2.length - 2; i>=0; i--) {
-				if (!chatInfo.addMessage(topic3, messages2[i])) {
-					System.out.println("ERROR: Couldnt add message!");
-					break;
-				}				
-			}
-			// Create String[] for the gui
-			// ("messages=String1;String2;String3;")
-			System.out.println(chatInfo.getMessages(topic3));
-			String messages3 = chatInfo.getMessages(topic3).split("=")[1];
-			String[] stringArray2 = messages3.split(";;");
-
-			// Check if should update the gui
-			if (gui.getCurrentTopic().equals(topic3)) {
-				gui.updateMessages(stringArray2);
-			}
-
-			break;
-		default:
-			throw new IllegalArgumentException("Invalid key: " + key);
-		}
-	}
-
+	/*
+	 * public void handleResponse(String input) {
+	 * System.out.println("serverResponse:" + input);
+	 * 
+	 * // Handle empty response if (input.equals("topics=") ||
+	 * input.equals("participants=")) { return; }
+	 * 
+	 * String key = input.split("=")[0]; String value = input.split("=")[1];
+	 * 
+	 * switch (key) { case "topics": // FORMAT: "topics=Topic1;Topic2;" String[]
+	 * topics = value.split(";"); for (int i = 0; i < topics.length; i++) {
+	 * chatInfo.addTopic(topics[i]); } gui.updateTopics(topics); break; case
+	 * "participants": // FORMAT: "participants=Peter;Paul;Mary;" String[] parts
+	 * = value.split(";"); for (int i = 0; i < parts.length; i++) {
+	 * participantInfo.addParticipant(parts[i]); }
+	 * gui.updateParticipants(parts); break; case "messages": // FORMAT:
+	 * "messages=Meldung1;Meldung2;" String[] messages =
+	 * input.split("=")[1].split(";;"); for (int i = messages.length - 2; i >=
+	 * 0; i--) { if (!chatInfo.addMessage(topic, messages[i])) {
+	 * System.out.println("ERROR: Couldnt add message!"); break; } } // Create
+	 * String[] for the gui // ("messages=String1;String2;String3;")
+	 * System.out.println(chatInfo.getMessages(topic)); String messages3 =
+	 * chatInfo.getMessages(topic).split("=")[1]; String[] stringArray2 =
+	 * messages3.split(";;");
+	 * 
+	 * // Check if should update the gui if
+	 * (gui.getCurrentTopic().equals(topic)) { gui.updateMessages(stringArray2);
+	 * }
+	 * 
+	 * break; default: throw new IllegalArgumentException("Invalid key: " +
+	 * key); } }
+	 */
 	// --------------------------------
 	// IChatDriver-Interface-Functions
 	// --------------------------------
@@ -216,15 +176,22 @@ public class Client implements IChatDriver, IChatRoom {
 
 	@Override
 	public boolean addParticipant(String name) throws IOException {
-		System.out.println("addParticipant SHOULD NEVER BE CALLED");
-		return true;
+		if (!name.trim().equalsIgnoreCase("")) {
+			makeRequest("?action=addParticipant&name="
+					+ URLEncoder.encode(clientName, "UTF-8"));
+			getParticipants();
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean removeParticipant(String name) throws IOException {
 		if (!name.trim().equalsIgnoreCase("")) {
 			System.out.println("Participant removed");
-		//	out.println("remove_name=" + name);
+			makeRequest("?action=removeParticipant&name="
+					+ URLEncoder.encode(clientName, "UTF-8"));
 			return true;
 		} else {
 			return false;
@@ -235,19 +202,22 @@ public class Client implements IChatDriver, IChatRoom {
 	public boolean addTopic(String topic) throws IOException {
 		if (!topic.trim().equalsIgnoreCase("")) {
 			System.out.println("Topic added: " + topic);
-		//	out.println("add_topic=" + topic);
+			makeRequest("?action=addTopic&topic="
+					+ URLEncoder.encode(topic, "UTF-8"));
+			getTopics();
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	
 	@Override
 	public boolean removeTopic(String topic) throws IOException {
 		if (!topic.trim().equalsIgnoreCase("")) {
 			System.out.println("Topic removed");
-		//	out.println("remove_topic=" + topic);
+			makeRequest("?action=removeTopic&topic="
+					+ URLEncoder.encode(topic, "UTF-8"));
+			getTopics();
 			return true;
 		} else {
 			return false;
@@ -259,7 +229,10 @@ public class Client implements IChatDriver, IChatRoom {
 		if (!topic.trim().equalsIgnoreCase("")
 				&& !message.trim().equalsIgnoreCase("")) {
 			System.out.println("Message added");
-		//	out.println("message=" + message + ";topic=" + topic);
+			makeRequest("?action=postMessage&message="
+					+ URLEncoder.encode(clientName + " : " + message, "UTF-8")
+					+ "&topic=" + URLEncoder.encode(topic, "UTF-8"));
+			getMessages(topic);
 			return true;
 		} else {
 			return false;
@@ -268,17 +241,61 @@ public class Client implements IChatDriver, IChatRoom {
 
 	@Override
 	public String getMessages(String topic) throws IOException {
-		System.out.println("getMessages CALLED--------------");
-		if (!topic.trim().equalsIgnoreCase("")
-				&& !chatInfo.getMessages(topic).equals("messages=")) {
-			String messages = chatInfo.getMessages(topic).split("=")[1];
-			String[] stringArray = messages.split(";;");
-			gui.updateMessages(stringArray);	// This is ugly but it works
-
+		System.out.println("getMessages CALLED: " + topic);
+		if (topic == null) {
+			return ("messages=");
+		}
+		if (!topic.trim().equalsIgnoreCase("")) {
+			String response = makeRequest("?action=getMessages&topic="
+					+ URLEncoder.encode(topic, "UTF-8"));
+			// FORMAT: "messages=Meldung1;Meldung2;"
+			System.out.println("response:" + response);
+			chatInfo.removeTopic(topic);
+			chatInfo.addTopic(topic);
+			if (!response.equals("messages=")) {
+				String[] messages = response.split("=")[1].split(";;");
+				for (int i = messages.length - 1; i >= 0; i--) {
+					if (!chatInfo.addMessage(topic, messages[i])) {
+						System.out.println("ERROR: Couldnt add message!");
+						break;
+					}
+				}
+			}
 			return chatInfo.getMessages(topic);
 		} else {
-			gui.updateMessages(new String[0]);
 			return ("messages=");
+		}
+	}
+
+	// FORMAT: "topics=Topic1;Topic2;"
+	public void getTopics() {
+		String response = client.makeRequest("?action=getTopics");
+		System.out.println("response:" + response);
+
+		if (!response.equals("topics=")) {
+			String value = response.split("=")[1];
+			String[] topics = value.split(";");
+			for (int i = 0; i < topics.length; i++) {
+				System.out.println("topics[i]:" + topics[i]);
+				client.chatInfo.addTopic(topics[i]);
+			}
+			client.gui.updateTopics(topics);
+		}
+	}
+
+	// FORMAT: "participants=Mary;Pete;"
+	public void getParticipants() {
+		String response = client.makeRequest("?action=getParticipants");
+		System.out.println("response:" + response);
+
+		if (!response.equals("participants=")) {
+			String value = response.split("=")[1];
+			String[] participants = value.split(";");
+			for (int i = 0; i < participants.length; i++) {
+				System.out.println("participant[i]:" + participants[i]);
+				client.participantInfo.addParticipant(participants[i]);
+			}
+			client.gui.updateParticipants(participants);
 		}
 	}
 
@@ -286,6 +303,9 @@ public class Client implements IChatDriver, IChatRoom {
 	public String refresh(String topic) throws IOException {
 		System.out.println("refresh CALLED--------------");
 		if (!topic.trim().equalsIgnoreCase("")) {
+			client.getParticipants();
+			client.getTopics();
+			client.getMessages(topic);
 			return chatInfo.getMessages(topic);
 		} else {
 			return ("messages=");
